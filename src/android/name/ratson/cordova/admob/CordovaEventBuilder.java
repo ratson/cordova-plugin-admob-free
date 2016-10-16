581179d44d18b45cfec6f035a0e0d32f86bec61c
Ratson <github@ratson.name>
2016-10-16 15:52:10 +0800

    Add CordovaEventBuilder

---
+++ src/android/name/ratson/cordova/admob/CordovaEventBuilder.java
@@ -0,0 +1,43 @@
+package name.ratson.cordova.admob;
+
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONObject;
+
+public class CordovaEventBuilder {
+    private String eventName;
+    private String jsonData;
+
+    public CordovaEventBuilder(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public CordovaEventBuilder withData(String data) {
+        this.jsonData = data;
+        return this;
+    }
+
+    public CordovaEventBuilder withData(JSONObject jsonObj) {
+        if (jsonObj == null) {
+            return withData("");
+        }
+        return withData(jsonObj.toString());
+    }
+
+    public String build() {
+        StringBuilder js = new StringBuilder();
+        js.append("javascript:cordova.fireDocumentEvent('");
+        js.append(eventName);
+        js.append("'");
+        if (jsonData != null && !"".equals(jsonData)) {
+            js.append(",");
+            js.append(jsonData);
+        }
+        js.append(");");
+        return js.toString();
+    }
+
+    public void fire(CordovaWebView webView) {
+        String js = build();
+        webView.loadUrl(js);
+    }
+}
---
+++ tests/android/app/src/test/java/name/ratson/testapplication/CordovaEventBuilderTest.java
@@ -0,0 +1,44 @@
+package name.ratson.testapplication;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import name.ratson.cordova.admob.CordovaEventBuilder;
+
+import static org.junit.Assert.assertEquals;
+
+public class CordovaEventBuilderTest {
+    @Test
+    public void buildEvent() {
+        String js = new CordovaEventBuilder("event").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event');");
+    }
+
+    @Test
+    public void ignoreEmptyData() {
+        String js = new CordovaEventBuilder("event").withData("").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event');");
+    }
+
+    @Test
+    public void buildEventWithData() {
+        String js = new CordovaEventBuilder("event").withData("{}").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{});");
+    }
+
+    @Test
+    public void buildEventWithJSONObject() throws JSONException {
+        JSONObject jsonObj = new JSONObject("{\"data\": 1}");
+        assertEquals(jsonObj.toString(), "{\"data\":1}");
+        CordovaEventBuilder builder = new CordovaEventBuilder("event").withData(jsonObj);
+        String js = builder.build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{\"data\":1});");
+
+
+        jsonObj = new JSONObject();
+        assertEquals(jsonObj.toString(), "{}");
+        js = builder.withData(jsonObj).build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{});");
+    }
+}
