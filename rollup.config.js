e321dca41d936d128049ca0949aba4f1d817d618
Ratson <github@ratson.name>
2016-12-23 22:12:53 +0800

    Use rollup

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'src/js/admob.js',
+  external: [
+    'cordova/exec',
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      externalHelpers: false,
+      externalHelpersWhitelist: [
+        'classCallCheck',
+        'createClass',
+        'extends',
+        'instanceof',
+        'typeof',
+      ],
+      babelrc: false,
+      presets: [
+        ['env', {
+          targets: {
+            browsers: [
+              '> 1%',
+              'last 4 versions',
+              'Android > 2',
+              'last 2 ChromeAndroid versions',
+            ],
+          },
+          modules: false,
+        }],
+        'stage-2',
+      ],
+      plugins: [
+        'add-module-exports',
+        'external-helpers',
+        'transform-es3-member-expression-literals',
+        'transform-es3-property-literals',
+        'transform-object-assign',
+        ['module-resolver', {
+          alias: {
+            lodash: 'lodash-es',
+          },
+        }],
+      ],
+    }),
+    nodeResolve({
+      jsnext: true,
+      browser: true,
+    }),
+    // commonjs(),
+    // uglify(),
+  ],
+  format: 'cjs',
+  sourceMap: true,
+}
